ebf1057a6cb6502a3291912fe655572d9975c953
daki-atahwalp-antango-boad <144264104+daki-atahwalp-antango-boad@users.noreply.github.com>
2023-09-20 17:29:12 +0000

    gotta fix this

--- MicroPython/main.py
+++ MicroPython/main.py
@@ -1,10 +1,22 @@
 """
-Created by: Mr. Coxall
-Created on: Sep 2020
-This module is a Micro:bit MicroPython program
+Created by: Daki A.B
+Created on: Sep 2023
+This module does basic math
 """
 
 from microbit import *
+from time import sleep
 
 
-display.scroll("Hello, World!")
+display.clear
+sleep(1)
+
+display.scroll("A = 3cm x 5cm = " + str(3 * 5) + display.scroll ("cm^2"))
+sleep(0.5)
+display.clear()
+
+display.scroll("P = 2cm(3cm x 5cm) = " + str(2 * (3 * 5)) + display.scroll ("cm"))
+sleep(0.5)
+display.clear()
+display.image(Image.HAPPY)
+
