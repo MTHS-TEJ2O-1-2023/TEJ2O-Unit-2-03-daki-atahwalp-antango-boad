32542dadbb4e8714e65fe290c386c11218bafd5b
daki-atahwalp-antango-boad <144264104+daki-atahwalp-antango-boad@users.noreply.github.com>
2023-09-20 12:57:20 -0400

    Microbit does math v4

--- main.ts
+++ main.ts
@@ -8,10 +8,10 @@
 basic.clearScreen()
 basic.pause(1000)
 
-basic.showString('A = 3cm x 5cm ='+ (3 * 5).toString() + 'cm^2')
+basic.showString('A = 3cm x 5cm =' + (3 * 5).toString() + 'cm^2')
 
 basic.clearScreen()
 basic.pause(1000)
 
-basic.showString('P = 2cm(3cm + 5cm) =' + (2*(3 + 5)).toString() + 'cm')
+basic.showString('P = 2cm(3cm + 5cm) =' + (2 * (3 + 5)).toString() + 'cm')
 basic.showIcon(IconNames.Happy)
