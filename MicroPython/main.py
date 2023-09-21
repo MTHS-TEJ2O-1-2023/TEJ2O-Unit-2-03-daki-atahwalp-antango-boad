e74e5fe9a918c70a40ede58e9c2a9fa02c4641a1
daki-atahwalp-antango-boad <144264104+daki-atahwalp-antango-boad@users.noreply.github.com>
2023-09-21 03:06:14 +0000

    didn't fix it :(

--- MicroPython/main.py
+++ MicroPython/main.py
@@ -8,15 +8,19 @@ from microbit import *
 from time import sleep
 
 
-display.clear
 sleep(1)
+display.clear()
 
-display.scroll("A = 3cm x 5cm = " + str(3 * 5) + display.scroll ("cm^2"))
+display.scroll("A = 3cm x 5cm = " + str(3 * 5))
+display.scroll("cm^2")
 sleep(0.5)
 display.clear()
 
-display.scroll("P = 2cm(3cm x 5cm) = " + str(2 * (3 * 5)) + display.scroll ("cm"))
+display.scroll("P = 2cm(3cm x 5cm) = " + str(2 * (3 * 5)))
+display.scroll("cm")
+display.show(Image.HAPPY)
 sleep(0.5)
 display.clear()
-display.image(Image.HAPPY)
+
+
 
